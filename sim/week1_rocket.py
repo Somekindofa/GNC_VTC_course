3409ee060242f1490a8dff19abc9ef5c25dd2c54
Somekindofa <akbas.theo@protonmail.com>
2026-04-28 13:07:55 +0200

    Add error calculations for height and speed in rocket simulation

--- sim/week1_rocket.py
+++ sim/week1_rocket.py
@@ -35,7 +35,13 @@ for t in range(len(time) - 1):
     v_euler[t + 1] = v_euler[t] + dt * (T - m * g) / m
     h_euler[t + 1] = h_euler[t] + dt * v_euler[t]
 
-# Speed
+## Errors 
+# Here, mean relative error, not the best metric as it doesn't take into account 
+# major errors.
+# Best would be peak error to show
+error_height = abs(np.nanmean((np.array(h) - np.array(h_euler))/np.array(h)))*100
+error_speed = abs(np.nanmean((np.array(v) - np.array(v_euler))/np.array(v)))*100
+
 fig, ax = plt.subplots(2, 1, figsize=(10, 6), sharex=True, tight_layout=True, dpi=100)
 ax[0].plot(time, v, color="red", lw=2, label="Speed Analytical")
 ax[0].plot(time, v_euler, color="blue", lw=1, ls="--", label="Speed Euler")
@@ -43,6 +49,13 @@ ax[0].plot(time, v_euler, color="blue", lw=1, ls="--", label="Speed Euler")
 ax[0].set_xlabel("Time (s)")
 ax[0].set_ylabel("Speed")
 ax[0].legend(loc="upper left")
+ax[0].text(
+    0.95, 0.08,
+    f"Error: {error_height:.3f}%",
+    transform=ax[0].transAxes,
+    ha="right", va="bottom",
+    bbox=dict(boxstyle="round", fc="white", alpha=0.7)
+)
 
 # Height
 ax[1].plot(time, h, color="red", lw=2, label="Height Analytical")
@@ -53,9 +66,3 @@ ax[1].set_ylabel("Height")
 ax[1].legend(loc="upper left")
 mplcursors.cursor(hover=True)
 plt.show()
-
-error_height = abs(np.nanmean((np.array(h) - np.array(h_euler))/np.array(h)))*100
-error_speed = abs(np.nanmean((np.array(v) - np.array(v_euler))/np.array(v)))*100
-
-print("Error% Height:", error_height)
-print("Error% Speed:", error_speed)
